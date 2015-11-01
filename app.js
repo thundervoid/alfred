0107b44618ea0165c84a3575b44fbae3f73e17d5
thundervoid <dil.buttar345@gmail.com>
2015-11-01 15:59:41 -0800

    Update app.js

--- app.js
+++ app.js
@@ -183,15 +183,8 @@ app.post('/', function (req, res, next) {
         if (req.body.sender_id !== '19982886') return next();
         var spam = req.body.text.split(req.body.text.match(/\bspam /i)[0])[1].trim();
         req.reply = [];
-        for (var i = 0; i < 1000; i++)
+        for (var i = 0; i < 20; i++)
         req.reply[i] = spam.charAt(0).toUpperCase() + spam.substring(1);
-            {
-            if (req.body.text.match(/^alfred(,)? stop[.!?]?$/i)) {
-                break;
-            }
-            break;
-            }
-            break;
         return next();    
         
     }
